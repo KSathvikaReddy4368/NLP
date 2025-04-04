79a17b312f94ecd2627a04e3512242f47e5dc860
SathvikaReddy <186664786+KSathvikaReddy4368@users.noreply.github.com>
2025-04-04 10:20:19 +0530

    NLP1

--- WORDANALYZER.py
+++ WORDANALYZER.py
@@ -2,12 +2,11 @@ import spacy
 from nltk.stem import PorterStemmer
 import nltk
 from nltk.tokenize import word_tokenize
-from nltk.tag import StanfordNERTagger
 nltk.download("punkt")
 nltk.download('punkt_tab')
 nlp = spacy.load("en_core_web_sm")
 stemmer=PorterStemmer()
-text="the runner was running quickly towards the finish line o claim the prize."
+text="sathvika is going to college  which is located at melumoi."
 tokens=word_tokenize(text)
 print(tokens)
 docs=nlp(text)
@@ -21,13 +20,5 @@ print("------------stemming-----------")
 tokenizer = nltk.word_tokenize(text)
 for token in tokenizer:
     print(token,stemmer.stem(token))
-print("------------NER-----------")
-stanford_ner_tagger = StanfordNERTagger(
-    '/path/to/stanford-ner/stanford-ner.jar',
-    '/path/to/stanford-ner/classifiers/english.all.3class.distsim.crf.ser.gz'
-)
-
-tokens = nltk.word_tokenize(text)
-classified_text = stanford_ner_tagger.tag(tokens)
-
-print(classified_text)
+for ent in docs.ents:
+    print(ent.text,ent.label_)
