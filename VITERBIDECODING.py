9fa04efbecfe884ba0f3944585dba2f9662f3b3c
SathvikaReddy <186664786+KSathvikaReddy4368@users.noreply.github.com>
2025-04-04 10:31:30 +0530

    NLP7

--- VITERBIDECODING.py
+++ VITERBIDECODING.py
@@ -1,42 +1,32 @@
-import nltk
-from nltk.corpus import treebank
-from nltk.tag import hmm
-
-# Train HMM tagger on Treebank corpus
-nltk.download('treebank')
-train_sents = treebank.tagged_sents()[:3000]
-tagger = hmm.HiddenMarkovModelTrainer().train(train_sents)
-
-# Define Viterbi Decoding function
-def viterbi_decode(sentence, tagger):
-    states = tagger._states
-    transitions = tagger._transitions
-    emissions = tagger._emissions
-    V = [{}]
-    path = {}
-
+def viterbi_decode(obs, states, start_prob, trans_prob, emit_prob):
+    V, path = [{}], {}
     for state in states:
-        V[0][state] = transitions['START'].get(state, 0) * emissions[state].get(sentence[0], 0)
+        V[0][state] = start_prob[state] * emit_prob[state].get(obs[0], 0)
         path[state] = [state]
-
-    for t in range(1, len(sentence)):
+    
+    for t in range(1, len(obs)):
         V.append({})
         new_path = {}
         for state in states:
-            (prob, state_max) = max(
-                (V[t-1][prev_state] * transitions[prev_state].get(state, 0) * emissions[state].get(sentence[t], 0), prev_state)
-                for prev_state in states
-            )
+            (prob, prev_state) = max(
+                (V[t-1][prev] * trans_prob[prev].get(state, 0) * emit_prob[state].get(obs[t], 0), prev) 
+                for prev in states)
             V[t][state] = prob
-            new_path[state] = path[state_max] + [state]
+            new_path[state] = path[prev_state] + [state]
         path = new_path
+    
+    final_state = max((V[len(obs)-1][state], state) for state in states)
+    return path[final_state[1]]
 
-    (prob, state_max) = max((V[len(sentence)-1][state], state) for state in states)
-    return path[state_max]
+# Example
+states = ['DT', 'NN', 'VB']
+observations = ['The', 'dog', 'barks']
+start_prob = {'DT': 0.6, 'NN': 0.3, 'VB': 0.1}
+trans_prob = {'DT': {'NN': 0.5}, 'NN': {'VB': 0.4}, 'VB': {'DT': 0.1}}
+emit_prob = {'DT': {'The': 0.9}, 'NN': {'dog': 0.8}, 'VB': {'barks': 0.7}}
 
-# Test sentence
-test_sentence = "This is a simple sentence".split()
+tags = viterbi_decode(observations, states, start_prob, trans_prob, emit_prob)
 
-# Apply Viterbi Decoding
-tagged_sentence = viterbi_decode(test_sentence, tagger)
-print(list(zip(test_sentence, tagged_sentence)))
+# Output the sentence with tags
+tagged_sentence = list(zip(observations, tags))
+print("Tagged Sentence:", tagged_sentence)
